be0a2fbf99b46f82f4e11e2ef6ad2dc20ae3c7dd
alexanderthenotsobad-git <alex.gomez.usf@outlook.com>
2024-07-15 15:03:24 +0000

    Initial Commit after wrangler (CF) config

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,1 +1,9 @@
-console.log('Hello from Worker');
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request));
+  });
+  
+  async function handleRequest(request) {
+    // Your request handling logic goes here
+    return new Response('Hello, World!', { status: 200 });
+  }
+  
